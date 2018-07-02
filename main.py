d328642069ee6b75458ce971cfea50cbb9034a8f
TaPO4eg3D <fo.denis2011@gmail.com>
2018-07-02 14:12:35 +0700

    Version 0.1 is finished

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
-import sys
+import os, sys, subprocess
 from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
@@ -13,29 +13,44 @@ ROOT_URL = 'http://horriblesubs.info'
 ALL_SHOWS = ROOT_URL + '/shows/'
 API_URL = 'https://horriblesubs.info/api.php?method=getshows&type=show&showid={}&nextid={}'
 
+
+def open_magnet(magnet):
+    """Open magnet according to os."""
+    if sys.platform.startswith('linux'):
+        subprocess.Popen(['xdg-open', magnet],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    elif sys.platform.startswith('win32'):
+        os.startfile(magnet)
+    elif sys.platform.startswith('cygwin'):
+        os.startfile(magnet)
+    elif sys.platform.startswith('darwin'):
+        subprocess.Popen(['open', magnet],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    else:
+        subprocess.Popen(['xdg-open', magnet],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
 class AnimeShow(QListWidgetItem):
     
-    def __init__(self, show_id, title):
+    def __init__(self, show_link, title):
         super().__init__(title)
-        self.show_id = show_id
+        self.show_link = show_link
         self.title = title
-    
-    @property
-    def tuple(self):
-        return (self.title, self.show_id)
 
     def setText(self):
         return self.title
 
     def __str__(self):
-        return '{} - {}'.format(self.title, self.show_id)
+        return '{} - {}'.format(self.title, self.show_link)
 
     def __repr__(self):
-        return '{} - {}'.format(self.title, self.show_id)
+        return '{} - {}'.format(self.title, self.show_link)
 
-class Episode:
+class Episode(QListWidgetItem):
     
     def __init__(self, title, magnet):
+        super().__init__(title)
         self.title = title
         self.magnet = magnet
     
@@ -46,7 +61,14 @@ class Episode:
         return '{} - {}'.format(self.title, self.magnet)
 
 
-def get_episodes(show_id, quality=1080):
+def get_episodes(show, quality=1080):
+    
+    html = requests.get(ROOT_URL + show['href']).text
+    soup = BeautifulSoup(html, 'lxml')
+    main_div = soup.find('div', class_='entry-content')
+    script_block = main_div.find('script').text
+    show_id = re.findall('\d+', script_block)[0]
+
     next_iter = 0
     result = list()
     while True:
@@ -57,11 +79,15 @@ def get_episodes(show_id, quality=1080):
             return result
         links = soup.find_all(class_='rls-info-container')
         for link in links:
+
             quality_block = link.find('div', class_='link-{}p'.format(quality))
             _link = quality_block.find(title='Magnet Link')
-            _title = link.get('id')
-            episode = Episode(_title, _link.get('href'))
+
+            title = '{} - {}'.format(show.get('title'), link.get('id'))
+
+            episode = Episode(title, _link.get('href'))
             result.append(episode)
+
         next_iter += 1
 
 def matched_shows(search):
@@ -72,13 +98,7 @@ def matched_shows(search):
     _matched_shows = main_div.find_all('a', title=re.compile('(?i){}'.format(search)))
     result = list()
     for show in _matched_shows:
-        html = requests.get(ROOT_URL + show['href']).text
-        soup = BeautifulSoup(html, 'lxml')
-        main_div = soup.find('div', class_='entry-content')
-        script_block = main_div.find('script').text
-        show_id = re.findall('\d+', script_block)[0]
-
-        anime_show = AnimeShow(show_id, show.text)
+        anime_show = AnimeShow(show, show.text)
         result.append(anime_show)
     return result
 
@@ -87,26 +107,42 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     
     def __init__(self):
         super().__init__()
-        self.setupUi(self);
+        self.setupUi(self)
+        self.animeView.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.searchButton.clicked.connect(self.fill_table)
         self.showEpisodes.clicked.connect(self.display_episodes)
+        self.downloadButton.clicked.connect(self.download_selected)
+        self.selectAll.clicked.connect(self.select_all)
+        self.deselectAll.clicked.connect(self.deselect_all)
     
     def fill_table(self):
         self.animeView.clear()
         if(self.searchField.toPlainText() == ''):
             return
         shows = matched_shows(self.searchField.toPlainText())
-        print(shows)
         for show in shows:
             self.animeView.addItem(show)
 
     def display_episodes(self):
         selected_item = self.animeView.selectedItems()[0]
-        print(selected_item.show_id)
+        episodes = get_episodes(selected_item.show_link)
+        self.animeView.clear()
+        for episode in episodes:
+            self.animeView.addItem(episode)
+    
+    def download_selected(self):
+        items = self.animeView.selectedItems()
+        for item in items:
+            open_magnet(item.magnet)
+    
+    def select_all(self):
+        self.animeView.selectAll()
+    
+    def deselect_all(self):
+        self.animeView.clearSelection()
 
 if __name__ == "__main__":
-    print(get_episodes(1075))
-    # app = QApplication(sys.argv)
-    # window = MainWindow()
-    # window.show()
-    # sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
